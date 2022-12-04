2e77d025387110d817b1f5018ad8d9299cf0aafa
Alexander Rezaei <alexander@Alexanders-MacBook-Pro.local>
2022-12-04 18:46:29 +0100

    final

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,21 +1,37 @@
 import React from "react";
 import img1 from "../assets/georgia-de-lotz-rncny1536Xs-unsplash.jpg";
+import { Link } from "react-router-dom";
+import { getToken } from "./storage/Storage";
+import { useNavigate } from "react-router-dom";
 function Cta() {
+    const navigate = useNavigate()
+    const token = getToken()
+
+    const redirectForPosts = () => {
+        if(token.length < 1) {
+            navigate("/login")
+        } else {
+            navigate("/allposts")
+        }
+    }
+
+    const redirectForProfiles = () => {
+        if(token.length < 1) {
+            navigate("/login")
+        } else {
+            navigate("/profiles")
+        }
+    }
     return (
-        <div className="flex">
-            <div className="w-[50%]">
+        <div className="flex flex-col md:flex-row">
+            <div className="w-[100%] md:w-[50%]">
                 <h2>View fresh posts from our users!</h2>
-                <p>
-                    Check out what our users have been posting by visiting our
-                    link below. or try searching for specific users by either id
-                    or name!
-                </p>
-                <div>
-                    <button>View posts</button>
-                    <button>View profiles</button>
+                <div className="mt-[30px] mb-[30px] md:mb-[0px]">
+                    <button onClick={redirectForPosts} className="md:mb-[20px]">View posts</button>
+                    <button onClick={redirectForProfiles}>View profiles</button>
                 </div>
             </div>
-            <div className="w-[50%]">
+            <div className="w-[100%] md:w-[50%]">
                 <img src={img1} />
             </div>
         </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,25 @@
 import React from "react";
-
+import { getToken } from "./storage/Storage";
+import { useNavigate } from "react-router-dom";
 function Hero() {
+    const navigate = useNavigate();
+    const token = getToken();
+
+    const redirectForLogin = () => {
+        if(token.length < 1) {
+            navigate("/login")
+        } else {
+            navigate("/")
+        }
+    }
+
+    const redirectForRegister = () => {
+        if(token.length < 1) {
+            navigate("/register")
+        } else {
+            navigate("/")
+        }
+    }
     return (
         <div className="">
             <div
@@ -10,9 +29,9 @@ function Hero() {
                 <h1 className="mt-11 max-w-[590px] text-center">
                     Try out Social you today!
                 </h1>
-                <div className="mt-11">
-                    <button className="mr-[37px]">Login</button>
-                    <button>signup</button>
+                <div className="mt-11 flex flex-col items-center sm:flex-row">
+                    <button onClick={redirectForLogin} className="mr-[0px] mb-[20px] sm:mr-[37px] sm:mb-[0px]">Login</button>
+                    <button onClick={redirectForRegister}>signup</button>
                 </div>
             </div>
         </div>
